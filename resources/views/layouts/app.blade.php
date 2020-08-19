dba4f487509158756dfd3ef5fd0463c320074583
damden92icc <61845774+damden92icc@users.noreply.github.com>
2020-08-19 21:17:30 +0200

    Controller Artists + Templates + routes

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Projet Reservations - @yield('title')</title>
+</head>
+
+<body>
+
+<div class="container">
+@yield('content');
+</div>
+
+
+</body>
+
+</html> 
