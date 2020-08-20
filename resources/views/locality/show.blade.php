0deaabab8ff932b30f1439a1a667ff09e2cf9deb
damden92icc <61845774+damden92icc@users.noreply.github.com>
2020-08-20 15:03:46 +0200

    Controller Locality + template + route

--- resources/views/locality/show.blade.php
+++ resources/views/locality/show.blade.php
@@ -4,4 +4,11 @@
 
 @section('content')
 <h1> {{$locality->postal_code}} {{$locality->locality}} </h1>
+
+<ul>
+    @foreach($locality->locations as $location)
+        <li>{{ $location->designation }} </li>
+    @endforeach
+</ul>
+
 @endsection
