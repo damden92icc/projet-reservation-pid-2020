fcfe4489874e1d751bd66729ab9d05b4a9da02d2
damden92icc <61845774+damden92icc@users.noreply.github.com>
2020-08-24 15:01:30 +0200

    user updating ok , no securized

--- app/Http/Controllers/ProfilController.php
+++ app/Http/Controllers/ProfilController.php
@@ -10,17 +10,32 @@ class ProfilController extends Controller
     // show
 
     public function show(User $user){
-
-    // dd($user);
-
-        return view('profil.show', [
-            'login'=> $user['login'],
-            'firstname'=> $user['firstname'],
-            'lastname'=> $user['lastname'],
-            'email'=> $user['email'],
-            'langue'=> $user['langue'],
-             ]
+        return view('profil.show', compact('user')
         );
         
+    }
+
+    public function edit(User $user){
+            return view('profil.edit', compact('user')
+        );
+
+    }
+
+
+    public function update(User $user){  
+        $data = request()->validate([
+            'login' => 'max:255',
+            'email' => 'max:255',
+            'firstname' =>'max:255',
+            'lastname'=> 'max:255',
+            'langue'=>  'max:2',
+        ]);
+
+       // Check if user authen 
+       auth()->$user->update($data);
+
+        $newUser = $user;
+
+        return redirect()->route('my profil', ['user' => $user] );
     }
 }
---
+++ resources/views/profil/edit.blade.php
@@ -0,0 +1,122 @@
+@extends('layouts.app')
+
+@section('title', 'Editer monMon profil')
+
+@section('content')
+
+<h1> Editer votre profil </h1>
+
+<form method="POST" action="{{ route('profil update', ['user' => $user])  }}">
+                        @csrf
+
+                        @method('PATCH')
+
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Login') }}</label>
+
+                            <div class="col-md-6">
+                           
+                                <input id="login" type="text" class="form-control @error('login') is-invalid @enderror" name="login" placeholder="{{ old('login') ??  $user->login  }}" autocomplete="name" autofocus>
+
+                                @error('login')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+
+
+                 
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" placeholder="{{ $user->email}}" value="{{ old('email')   }}" autocomplete="email">
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" autocomplete="new-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation"  autocomplete="new-password">
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="firstname" class="col-md-4 col-form-label text-md-right">{{ __('Firstname') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="firstname" type="text" class="form-control @error('firstname') is-invalid @enderror" placeholder="{{ $user->firstname}}" name="firstname" value="{{ old('firstname')  }}" autocomplete="name" autofocus>
+
+                                @error('firstname')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="lastname" class="col-md-4 col-form-label text-md-right">{{ __('Lastame') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="lastname" type="text" class="form-control @error('lastname') is-invalid @enderror" placeholder="{{ $user->lastname}}" name="lastname" value="{{ old('lastname')  }}"  autocomplete="name" autofocus>
+
+                                @error('lastname')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="langue" class="col-md-4 col-form-label text-md-right">{{ __('Langue') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="langue" type="text" class="form-control @error('langue') is-invalid @enderror" name="langue" placeholder="{{ $user->langue}}" value="{{ old('langue') }}"  autocomplete="name" autofocus>
+
+                                @error('langue')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Editer') }}
+                                </button>
+                            </div>
+                        </div>
+
+
+                    </form>
+
+@endsection
